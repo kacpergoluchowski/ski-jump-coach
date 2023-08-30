d72e26590d4d762d295983dc24a73231a979aecf
Kacper Gołuchowski <kacper.goluchowski@o2.pl>
2023-08-30 11:03:40 +0200

    add env in react

--- src/components/SportsEvents.jsx
+++ src/components/SportsEvents.jsx
@@ -65,7 +65,7 @@ function ReportCompetitors(props) {
     async function fetchData(country) {
         try {
             setLoaderDisplay(true);
-            const response = await fetch('http://localhost:3737/getCompetitors', {
+            const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/getCompetitors`, {
                 method: "POST",
                 body: JSON.stringify({ country: country }),
                 headers: {
@@ -110,7 +110,7 @@ function ReportCompetitors(props) {
 
 function Competitors(props) {
     async function reportCompetitor(name, surname, skill, country) {
-        await fetch('http://127.0.0.1:3737/reportCompetitor', {
+        await fetch(`${process.env.REACT_APP_BACKEND_URL}/reportCompetitor`, {
             method: "POST",
             body: JSON.stringify({name: name, surname: surname, country: country, skill: skill}),
             headers: {
